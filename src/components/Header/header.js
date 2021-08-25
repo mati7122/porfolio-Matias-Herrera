80a3d5e5d7ffa98096adeea080f03ffb1fbbf277
Matías Herrera <matias7122@gmail.com>
2021-08-25 20:16:43 -0300

    header component was changed

--- src/components/Header/header.js
+++ src/components/Header/header.js
@@ -2,7 +2,12 @@
 import './Header.scss'
 
 function Header(){
-    return <div className="headerContent"></div>
+    // return <div className="headerContent"></div>
+    return(
+        <div className="headerContent">
+            <p>test</p>
+        </div>
+    );
 }
 
 export default Header;
