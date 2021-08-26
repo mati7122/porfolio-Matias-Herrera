8a06a246cb25337f15f01efa092f08168c5d3356
Matías Herrera <matias7122@gmail.com>
2021-08-26 20:58:21 -0300

    Header modified

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,12 @@
 
 const style = {
-    backgroundColor : '#5D6868',
+    backgroundColor: '#5D6868',
     width: '100%',
-    height: '10vh' 
+    height: '10vh'
 }
 
-function Header(){
-    return(
+function Header() {
+    return (
         <div style={style}>
         </div>
     );
