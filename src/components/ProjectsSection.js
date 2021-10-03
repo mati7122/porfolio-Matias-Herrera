d7ff79afcd80603fbb5cb11c4f7ca956f006c059
matias <matias7122@gmail.com>
2021-10-03 00:33:30 -0300

    added new article in project section

--- src/components/ProjectsSection.js
+++ src/components/ProjectsSection.js
@@ -2,6 +2,7 @@
 import ImgCRUD from '../assets/CRUD.gif';
 import API from '../assets/APIimage.jpg';
 import poke from '../assets/pokemon.jpg';
+import morty from '../assets/morty.gif';
 
 import ArticleProject from './ArticleProject';
 
@@ -15,7 +16,8 @@ const paragraph = {
     Aplicación desarrollada con NodeJS, utilizando Express y mongoose
   `,
     Pokemon: `Aplicación sencilla desarrollada con React y estilizada con css, consumiendo los datos 
-    de una API la cual contiene datos de la saga de videojuegos Pokémon`
+    de una API la cual contiene datos de la saga de videojuegos Pokémon`,
+    Rick: 'Otra aplicación sencilla utilizando la API de Rick y Morty, una serie de animación muy popular. Mediante la app se pueden buscar sus personajes obteniendo datos y sus imagenes'
 }
 
 function ProjectsSection() {
@@ -25,6 +27,7 @@ function ProjectsSection() {
             <ArticleProject title="CRUD STACK MERN" paragraph={paragraph.CRUD} img={ImgCRUD} GitHub="https://github.com/mati7122/CRUD-stack-MERN" link="https://crud-stack-mern.vercel.app" />
             <ArticleProject title="API REST CRUD STACK MERN" paragraph={paragraph.API} img={API} GitHub="https://github.com/mati7122/API-CRUD-Stack-MERN" link="https://api-crud-stack-mern.herokuapp.com" />
             <ArticleProject title="Pokémon APP" paragraph={paragraph.Pokemon} img={poke} GitHub="https://github.com/mati7122/pokedex" link='https://pokemon-red.vercel.app' />
+            <ArticleProject title="Rick & Morty APP" paragraph={paragraph.Rick} img={morty} GitHub="https://github.com/mati7122/rick-and-morty" link="https://rickandmorty-five.vercel.app"/>
         </div>
     );
 }
